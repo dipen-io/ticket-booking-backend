6ac4f213884e17551f7b46cd60c8663538b19500
Dipen Boro <borod9200@gmail.com>
2026-05-24 20:18:27 +0530

    prisma configuration

--- user-service/config/prisma.js
+++ user-service/config/prisma.js
@@ -1,8 +1,20 @@
-const { PrismaClient } = require("prisma/PrismaClient");
+const { PrismaClient } = require("@prisma/client");
 const { config } = require(".");
+const { PrismaPg } = require("@prisma/adapter-pg");
+
+const connectionString = process.env.DATABASE_URL;
 const globalForPrisma = global;
 
 const prisma = globalForPrisma.prisma ?? new PrismaClient({});
+
+if (!globalForPrisma.prisma) {
+  const adapter = new PrismaPg({ connectionString });
+
+  globalForPrisma.prisma = new PrismaClient({
+    adapter,
+    log: ["error", "warn"],
+  });
+}
 
 if (config.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
