c1c9d9b0931bdf67db8416169614e4986a0b2b31
Dipen Boro <borod9200@gmail.com>
2026-05-24 22:03:01 +0530

    emial sending demo

--- user-service/utils/email.js
+++ user-service/utils/email.js
@@ -4,23 +4,25 @@ const sgMail = require("@s");
 const minutes = (config.OTP_TTL || 300) / 60;
 
 async function sendOtpEmail(email, otp) {
-  const msg = {
-    to: email,
-    from: `${config.MAIL.SEND}`,
-    subject: "Your Mail",
-    html: `
+    const msg = {
+        to: email,
+        from: `${config.MAIL.SEND}`,
+        subject: "Your Mail",
+        html: `
 
       `,
-  };
-  await sgMail.send(msg);
+    };
+    await sgMail.send(msg);
 }
 
 async function verifyOtpEmail(meta) {
-  const msg = {
-    to: meta.email,
-    from: `${config.MAIL_SEND}`,
-    subject: "Email is Verfied",
-    html: ``,
-  };
-  await sgMail.send(msg);
+    const msg = {
+        to: meta.email,
+        from: `${config.MAIL_SEND}`,
+        subject: "Email is Verfied",
+        html: ``,
+    };
+    await sgMail.send(msg);
 }
+
+module.exports = { sendOtpEmail, verifyOtpEmail };
