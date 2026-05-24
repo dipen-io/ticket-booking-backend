95849ea649988df342f24a6e179cd90654b314fa
Dipen Boro <borod9200@gmail.com>
2026-05-24 22:03:12 +0530

    add bunch of errors

--- user-service/utils/error.js
+++ user-service/utils/error.js
@@ -1,43 +1,57 @@
 class AppError extends Error {
-  constructor(message, statusCode, code) {
-    super(message);
-    this.statusCode = statusCode;
-    this.code = code;
+    constructor(message, statusCode, code) {
+        super(message);
+        this.statusCode = statusCode;
+        this.code = code;
 
-    // Captures the stack trace and excludes this constructor from it
-    Error.captureStackTrace(this, this.constructor);
-  }
+        // Captures the stack trace and excludes this constructor from it
+        Error.captureStackTrace(this, this.constructor);
+    }
 }
 
 class BadRequestError extends AppError {
-  constructor(message, code = "BAD_REQUEST") {
-    super(message, 400, code);
-  }
+    constructor(message, code = "BAD_REQUEST") {
+        super(message, 400, code);
+    }
 }
 
 class UnauthorizedError extends AppError {
-  constructor(message, code = "UNAUTHORIZED") {
-    super(message, 401, code);
-  }
+    constructor(message, code = "UNAUTHORIZED") {
+        super(message, 401, code);
+    }
 }
 
 class ForbiddenError extends AppError {
-  constructor(message, code = "FORBIDDEN") {
-    super(message, 403, code);
-  }
+    constructor(message, code = "FORBIDDEN") {
+        super(message, 403, code);
+    }
 }
 
 class NotFoundError extends AppError {
-  constructor(message, code = "NOT_FOUND") {
-    super(message, 404, code);
-  }
+    constructor(message, code = "NOT_FOUND") {
+        super(message, 404, code);
+    }
+}
+
+class ConflictError extends AppError {
+    constructor(message, code = "CONFLICT_ERROR") {
+        super(message, 405, code);
+    }
+}
+
+class TooManyRequest extends AppError {
+    constructor(message, code = "TOO_MANY_REQUEST") {
+        super(message, 405, code);
+    }
 }
 
 // Corrected to module.exports and included all classes
 module.exports = {
-  AppError,
-  BadRequestError,
-  UnauthorizedError,
-  ForbiddenError,
-  NotFoundError,
+    AppError,
+    ConflictError,
+    TooManyRequest,
+    BadRequestError,
+    UnauthorizedError,
+    ForbiddenError,
+    NotFoundError,
 };
