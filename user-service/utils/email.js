d9339ebf202a61c80a5b14d56c7c147da7c8e3c2
Dipen Boro <borod9200@gmail.com>
2026-05-24 20:18:13 +0530

    mail sends utility

---
+++ user-service/utils/email.js
@@ -0,0 +1,26 @@
+const { config } = require("../config");
+
+const sgMail = require("@s");
+const minutes = (config.OTP_TTL || 300) / 60;
+
+async function sendOtpEmail(email, otp) {
+  const msg = {
+    to: email,
+    from: `${config.MAIL.SEND}`,
+    subject: "Your Mail",
+    html: `
+
+      `,
+  };
+  await sgMail.send(msg);
+}
+
+async function verifyOtpEmail(meta) {
+  const msg = {
+    to: meta.email,
+    from: `${config.MAIL_SEND}`,
+    subject: "Email is Verfied",
+    html: ``,
+  };
+  await sgMail.send(msg);
+}
