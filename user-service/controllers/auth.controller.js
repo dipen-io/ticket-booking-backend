532e8f5899624801c76e8b5a3786c5bbeda744e1
Dipen Boro <borod9200@gmail.com>
2026-05-24 23:02:10 +0530

    done controller for user creation

--- user-service/controllers/auth.controller.js
+++ user-service/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const asyncHandlers = require("../utils/asyncHandlers");
 const { AppError, BadRequestError } = require("../utils/error");
+const { config } = require("../config");
+const authService = require("../services/auth.service");
 
 exports.sendOTP = asyncHandlers(async (req, res) => {
     const { firstName, lastName, email, password, confirmPassword } = req.body;
@@ -9,4 +11,24 @@ exports.sendOTP = asyncHandlers(async (req, res) => {
     if (password !== confirmPassword) {
         throw new BadRequestError("Password mismatch");
     }
+
+    const { otpSessionId } = await authService.sendOTP(
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmPassword,
+    );
+
+    res.cookie("otp_session", otpSessionId, {
+        httonly: true,
+        secure: true,
+        samesite: "strict",
+        maxAge: config.OTP_TTL * 1000,
+    })
+        .status(200)
+        .json({
+            success: true,
+            message: "Otp Send Successfully",
+        });
 });
