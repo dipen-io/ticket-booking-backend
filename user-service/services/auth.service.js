52b395db187e93ab6cca7ad4ca7774de9f021fc5
Dipen Boro <borod9200@gmail.com>
2026-05-24 22:02:08 +0530

    services section started

---
+++ user-service/services/auth.service.js
@@ -0,0 +1,28 @@
+const prisma = require("../config/prisma");
+const { sendOtpEmail } = require("../utils/email");
+const { ConflictError } = require("../utils/error");
+const bcrypt = require("bcryptjs");
+const { generateAndStoreOtp } = require("../utils/otp");
+
+const sendOTP = async (
+    firstName,
+    lastName,
+    email,
+    password,
+    confirmPassword,
+) => {
+    const existingUser = await prisma.user.findUnique({
+        where: { email },
+    });
+
+    if (existingUser) {
+        throw new ConflictError("User already exist!");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const meta = { firstName, lastName, email, hashedPassword };
+    const { otp, otpSessionId } = await generateAndStoreOtp(meta);
+    await sendOtpEmail(email, otp);
+};
+
+module.exports = { sendOTP };
