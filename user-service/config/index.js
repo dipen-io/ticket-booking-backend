9633ab1ee381ec344176e6899278e50e8ed140ea
Dipen Boro <borod9200@gmail.com>
2026-05-25 15:11:25 +0530

    moved env to .secrets/shared.env

--- user-service/config/index.js
+++ user-service/config/index.js
@@ -1,4 +1,8 @@
-require("dotenv").config();
+// require("dotenv").config();
+const path = require("path");
+require("dotenv").config({
+    path: path.resolve(__dirname, "../../.secrets/shared.env"),
+});
 
 const config = {
     SERVICE_NAME: require("../package.json").name,
