fa2bae32a87204374925ebff8c24abf0fc257b82
Dipen Boro <borod9200@gmail.com>
2026-05-29 11:03:20 +0530

    adding device info for verification

---
+++ user-service/utils/getDeviceInfo.js
@@ -0,0 +1,16 @@
+const crypto = require("crypto");
+
+function getDeviceFignerPrint(req){
+    const userAgent =req.headers["user-agent"] || "";
+
+    const ip = req.ip || "";
+    const accept = req.headers["accept"] || "";
+
+    const raw = `${userAgent}|${ip}|${accept}`;
+
+    return crypto
+    .createHash("sha256")
+    .update(raw)
+    .digest('hex')
+
+}
